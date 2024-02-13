44e220ffb825197fe4f03cafd3e27bf1f050de7a
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-13 16:53:08 +0000

    update action server

--- ros2_dspy/ollama_inference.py
+++ ros2_dspy/ollama_inference.py
@@ -12,9 +12,14 @@ from rclpy.node import Node
 from rclpy.action import ActionServer
 
 import dspy
-#from ros2_dspy_msgs.action import DSPY
+from ros2_dspy_msgs.action import QA
 
 DOCKER_COMPOSE_PATH = get_package_share_directory('ros2_dspy') + '/.docker/docker-compose-ollama.yaml'
+
+class BasicQA(dspy.Signature):
+    """Answer common sense questions."""
+    question = dspy.InputField(desc="a question about solving a real-world manipulation task")
+    answer = dspy.OutputField(desc="a succinct answer to the question")
 
 class OllamaInferenceNode(Node):
     """Node for performing inference with Ollama and DSPy."""
@@ -30,33 +35,35 @@ class OllamaInferenceNode(Node):
 
         # start ollama inference server
         self.logger.info('Starting Ollama inference server...')
+        subprocess.run(f'ollama pull {model}', shell=True, check=True)
         subprocess.run(f'docker compose -f {DOCKER_COMPOSE_PATH} up -d --no-recreate', shell=True, check=True)
-        subprocess.run(f'docker exec ollama-inference-ollama-1 ollama run {model}', shell=True, check=True)
 
         # setup DSPy to interact with local Ollama server
-        ollama_model = dspy.OpenAI(
-                api_base='http://localhost:11434/v1/', 
-                api_key='ollama', 
+        ollama_model = dspy.OllamaLocal(
                 model=model,
-                stop='\n\n', 
                 model_type='chat'
                 )
         dspy.settings.configure(lm=ollama_model)
-
+        self.qa_predictor= dspy.Predict(BasicQA)
 
         # initialize action server for ollama inference
-        #self._ollama_predict_action_server = ActionServer(
-        #    self,
-        #    DSPY,
-        #    'ollama_predict',
-        #    self._ollama_predict_callback,
-        #    )
+        self._ollama_predict_action_server = ActionServer(
+            self,
+            QA,
+            'ollama_predict',
+            self.ollama_qa_callback,
+        )
 
-        def _ollama_predict_callback(self, goal_handle):
-            """Callback for ollama_predict action server."""
-            self.logger.info('Received ollama_predict action request.')
+    def ollama_qa_callback(self, goal_handle):
+        """Callback for ollama_predict action server."""
+        self.logger.info('Received ollama_predict action request.')
+        # TODO: add feedback to action server
+        pred = self.qa_predictor(question=goal_handle.request.question)
+        self.logger.info(f'Answer: {pred.answer}')
+        result = QA.Result()
+        result.answer = pred.answer
+        return result
             
-
 def main(args=None):
     rclpy.init(args=args)
     ollama_inference_node = OllamaInferenceNode()
